338193271a538704887789ed135195d8b09a413e
unknown <meredith.marcum@hotmail.com>
2021-08-01 15:14:48 -0500

    Made baseline design and created array with student info

---
+++ main.js
@@ -0,0 +1,41 @@
+const firstYears = [
+  {
+    id: 1,
+    name: "asher potter",
+    age: 11
+},
+
+{
+  id: 2,
+  name: "mckenna malfoy",
+  age: 12
+},
+
+{
+  id: 3,
+  name: "graham scamander",
+  age: 11
+},
+
+{
+  id: 4,
+  name: "amelia olivander",
+  age: 11
+},
+
+{
+  id: 5,
+  name: "maverick thomas",
+  age: 12
+},
+
+{
+  id: 6,
+  name: "hazel lupin",
+  age: 11
+}
+];
+
+for (i=0; i<Array.length; i++){
+ console.log(firstYears[i]);
+}
